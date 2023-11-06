593592a4ece7f837155d9e206a26d670a97ab5cd
RamnikDhoot <99842947+RamnikDhoot@users.noreply.github.com>
2023-11-06 18:55:44 +0000

    Created exception class for giving the calculator the wrong type

---
+++ BadTypeException.java
@@ -0,0 +1,23 @@
+package calculator;
+
+
+/**
+ * This is a custom Exception class thrown when trying to return the wrong type of value.
+ */
+public class BadTypeException extends Exception {
+
+  /**
+   * ID for the exception.
+   */
+  private static final long serialVersionUID = 1L;
+
+  /**
+   * A constructor for the exception.
+   * 
+   * @param message the message to be displayed when the exception is thrown
+   */
+  public BadTypeException(String message) {
+    super(message);
+  }
+
+}
