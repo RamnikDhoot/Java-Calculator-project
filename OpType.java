f45d86e3a8ba054461ba51b95881b398eff754db
RamnikDhoot <99842947+RamnikDhoot@users.noreply.github.com>
2023-11-06 18:54:12 +0000

    Created type for each different calculation

---
+++ OpType.java
@@ -0,0 +1,19 @@
+package calculator;
+
+/**
+* The ways in a which a calculator may evaluate an arithmetic expression.
+* 
+*
+*/
+public enum OpType {
+  
+/**
+* Normal order with operator between arguments like 5 * (3 + 2).
+*/
+INFIX,
+
+/**
+* Reverse Polish notation e.g. 325+*.
+*/
+POSTFIX;
+}
