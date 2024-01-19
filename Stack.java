4fd34c85e06f9bb2499b2c40f6cb323a5292d997
RamnikDhoot <99842947+RamnikDhoot@users.noreply.github.com>
2024-01-19 21:50:19 +0000

    Refactoring and encapsulating values

--- Stack.java
+++ Stack.java
@@ -5,77 +5,78 @@ import java.util.EmptyStackException;
 
 /**
  * This is an implementation of a stack of Entry objects.
- * 
- *
  */
 public class Stack {
 
-  /**
-   * The list of values in the stack.
-   */
-  ArrayList<Entry> values;
+    /**
+     * The list of values in the stack.
+     */
+    private ArrayList<Entry> values;
 
-  /**
-   * The size of the stack.
-   */
-  int size;
-
-  /**
-   * The constructor for a stack object.
-   */
-  public Stack() {
-    values = new ArrayList<Entry>();
-  }
-
-  /**
-   * Returns the size of the stack.
-   * 
-   * @return size of the stack.
-   */
-  public int size() {
-    return size;
-  }
-
-  /**
-   * Adds an entry to the top of the stack.
-   * 
-   * @param i the entry to be put on the stack.
-   */
-  public void push(Entry i) {
-    size += 1;
-    values.add(i);
-
-  }
-
-  /**
-   * Removes the newest entry added from the stack(top).
-   * 
-   * @return the entry that was removed.
-   * @throws EmptyStackException thrown when the stack is empty and we try to
-   *                             remove an entry.
-   */
-  public Entry pop() {
-    if (size == 0) {
-      throw new EmptyStackException();
+    /**
+     * The constructor for a stack object.
+     */
+    public Stack() {
+        values = new ArrayList<>();
     }
-    Entry pop = values.get(values.size() - 1);
-    values.remove(values.size() - 1);
-    size -= 1;
-    return pop;
-  }
 
-  /**
-   * The newest item added to the stack.
-   * 
-   * @return a copy of the item at the top of the stack.
-   * @throws EmptyStackException thrown when the stack is empty and we try to get
-   *                             the top entry.
-   */
-  public Entry top() {
-    if (size == 0) {
-      throw new EmptyStackException();
+    /**
+     * Returns the size of the stack.
+     * 
+     * @return size of the stack.
+     */
+    public int size() {
+        return values.size();
     }
-    return values.get(values.size() - 1);
-  }
 
+    /**
+     * Adds an entry to the top of the stack.
+     * 
+     * @param entry the entry to be put on the stack.
+     */
+    public void push(Entry entry) {
+        values.add(entry);
+    }
+
+    /**
+     * Removes the newest entry added from the stack (top).
+     * 
+     * @return the entry that was removed.
+     * @throws EmptyStackException thrown when the stack is empty.
+     */
+    public Entry pop() {
+        if (isEmpty()) {
+            throw new EmptyStackException();
+        }
+        return values.remove(values.size() - 1);
+    }
+
+    /**
+     * Returns the item at the top of the stack without removing it.
+     * 
+     * @return the item at the top of the stack.
+     * @throws EmptyStackException thrown when the stack is empty.
+     */
+    public Entry peek() {
+        if (isEmpty()) {
+            throw new EmptyStackException();
+        }
+        return values.get(values.size() - 1);
+    }
+
+    /**
+     * Checks if the stack is empty.
+     * 
+     * @return true if the stack is empty, false otherwise.
+     */
+    public boolean isEmpty() {
+        return values.isEmpty();
+    }
+
+    /**
+     * Clears the stack.
+     */
+    public void clear() {
+        values.clear();
+    }
 }
